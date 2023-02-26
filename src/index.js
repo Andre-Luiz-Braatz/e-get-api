09f5763ab9ec3611a29c592055bbd5e670066fa0
Andre <andreluiz.braatz@gmail.com>
2023-02-26 20:49:26 -0300

    feat: crud

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const md5 = require("md5");
+const cors = require("cors");
+// const md5 = require("md5");
 const PORT = 4000;
 const app = express();
 const Pool = require('pg').Pool
+
+app.use(cors())
+app.use(express.json());
 
 const pool = new Pool({
   user: 'postgres',
@@ -12,49 +16,125 @@ const pool = new Pool({
   port: 5432,
 })
 
-app.use(express.json());
-
-
 // Middleware
 
-function verifyIfExistsData(request, response, next) {
-  const {email,password} = request.body;
-  console.log("1: ",email,password);
-
+app.get("/signin", (request, response) => {
+  const {email,password} = request.query;
   if (!email || !password)
-    return response.status(500).json({ error: "Invalid data!" });
-  request.email = email;
-  request.password = password;
-  return next();
-}
-function verifyIfExistsUser(request, response, next) {
-  const {email} = request;
-  const sql = `SELECT * FROM "user" where email ilike '${email}'`;
-  pool.query(sql, (error, result) => {
-    console.log('error, result: ',email,error, result);
-    if (error) return response.status(401).json({msg: 'Unable to register!'});
-    else if(result && result.rows && result.rows[0] && result.rows[0].id) 
-      return response.status(401).json({msg: 'User already exists!'});
-    else next();
-  })
-}
+    return response.status(500).send();
 
-app.get("/signin",verifyIfExistsData, (request, response) => {
-  const {email,password} = request;
   const sql = `SELECT * FROM "user" where email ilike '${email}' and password ilike 
   '${password}'`;
   pool.query(sql, (error, result) => {
-    if (error) return response.status(401).json({msg: 'Unable to login!'});
-    else response.status(201).json(result.rows)  
+    if (error) return response.status(401).send();
+    else if(!result || !result.rows || !result.rows.length) response.status(401).send()
+    else {
+      delete result.rows[0].password
+      response.status(201).json(result.rows[0]);
+    }
   })
 });
 
-app.post("/signup",verifyIfExistsData,verifyIfExistsUser, (request, response) => {
-  const {email,password} = request;
+app.post("/signup", (request, response) => {
+  const {email,password} = request.body;
+  console.log(email,password);
+  if (!email || !password)
+    return response.status(500).json({ error: "Invalid data!" });
+
   const sql = `insert into "user" (email,password) values ('${email}','${password}') returning *`;
   pool.query(sql, (error, result) => {
-    if (error) return response.status(401).json({msg: 'Unable to register!'});
-    else response.status(201).json(result.rows)  
+    if (error) return response.status(401).send();
+    else if(!result || !result.rows || !result.rows.length) response.status(401).send()
+    else {
+      delete result.rows[0].password
+      response.status(201).json(result.rows[0]);
+    }
+  })
+});
+
+app.get("/category", (request, response) => {
+  const {user_id} = request.query;
+  if (!user_id)
+    return response.status(500).send();
+
+  const sql = `select * from category where user_id = ${user_id} order by created_at desc`;
+
+  pool.query(sql, (error, result) => {
+    if (error) return response.status(401).send();
+    else response.status(201).json(result.rows);
+  })
+});
+
+app.post("/category", (request, response) => {
+  const {user_id,name,description} = request.body;
+  if (!user_id || !name || !description)
+    return response.status(500).send();
+
+  const sql = `insert into category (user_id, name,description) 
+  values ('${user_id}','${name}','${description}') returning *`;
+
+  pool.query(sql, (error, result) => {
+    if (error) return response.status(401).send();
+    else response.status(201).json(result.rows);
+  })
+});
+app.put("/category/:id", (request, response) => {
+  const {user_id,name,description} = request.body;
+  const {id} = request.params;
+  if (!user_id || !name || !description || !id)
+    return response.status(500).send();
+
+  const sql = `update category set name = '${name}', description = '${description}' 
+  where id = ${id} and user_id = ${user_id}
+  returning *`;
+
+  pool.query(sql, (error, result) => {
+    if (error) return response.status(401).send();
+    else response.status(201).json(result.rows);
+  })
+});
+
+app.get("/product", (request, response) => {
+  const {user_id} = request.query;
+  if (!user_id)
+    return response.status(500).send();
+
+  const sql = `select product.*,category.name as category_name from product 
+  inner join category on category.id = product.category_id
+  where category.user_id = '${user_id}' order by created_at desc`;
+
+  pool.query(sql, (error, result) => {
+    if (error) return response.status(401).send();
+    else response.status(201).json(result.rows);
+  })
+});
+app.post("/product", (request, response) => {
+  const {user_id,category_id,name,description,value,stock} = request.body;
+  if (!user_id || !category_id || !name || !description || !value || !stock)
+    return response.status(500).send();
+
+  const sql = `insert into product (category_id,name,description,value,stock)
+  values ('${category_id}','${name}','${description}','${value}','${stock}') returning *`;
+
+  pool.query(sql, (error, result) => {
+    if (error) return response.status(401).send();
+    else response.status(201).json(result.rows);
+  })
+});
+
+app.put("/product/:id", (request, response) => {
+  const {user_id,category_id,name,description,value,stock} = request.body;
+  const {id} = request.params;
+  if (!user_id || !name || !description || !id)
+    return response.status(500).send();
+
+  const sql = `update product set name = '${name}', description = '${description}',
+  category_id = '${category_id}', value = '${value}',stock = '${stock}' 
+  where id = ${id}
+  returning *`;
+  pool.query(sql, (error, result) => {
+    if (error) return response.status(401).send();
+    else response.status(201).json(result.rows);
   })
 });
 
